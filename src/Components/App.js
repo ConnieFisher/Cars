b8adb3b2501ae5fc090d0a74085bec0bbc9357cf
ConnieFisher <ConnieFisher@users.noreply.github.com>
2020-02-28 16:10:59 -0500

    91

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 
@@ -6,13 +7,18 @@ class App extends React.Component {
   state = {};
 
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID bWbV24UyqfQnL9UxBJjb0XFo-FsHk0scUBX6mZLLgTM"
+      }
+    });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
