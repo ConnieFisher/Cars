c42f1523afe9dca629fc8a78eaae8f1b32d6f1ec
ConnieFisher <ConnieFisher@users.noreply.github.com>
2020-03-03 14:13:11 -0500

    100

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,2 +1,14 @@
 import React from 'react';
 
+const ImageList = (props) => {
+    const images = props.images.map(({id, urls, description}) => {
+        return <img key={id} src={urls.regular} alt={description} style={{ width: 200}}/>
+    })
+
+    return ( 
+
+    <div>{images}</div>
+     );
+}
+ 
+export default ImageList;
